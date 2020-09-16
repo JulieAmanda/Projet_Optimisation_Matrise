8db1b0bf8ce939d75d7aa0e17f523fc303a73426
Julie Amanda <julieamandakemoe@gmail.com>
2020-09-16 13:25:29 -0400

    Initial Commit

---
+++ Projet_v2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Projet_v2
+//
+//  Created by JULIE AMANDA TCHOUANDEM KEMOE on 2020-09-16.
+//  Copyright © 2020 JULIE AMANDA TCHOUANDEM KEMOE. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
